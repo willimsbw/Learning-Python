77adb5cce1d87d24ce3a5324055614d7bc6e95fc
Bryan Williams <Bryan.williams@achiever.com>
2018-03-23 13:19:35 -0400

    added for loop to make it wait 3 seconds then open youtube, and do it three times

--- Take-a-break-function.py
+++ Take-a-break-function.py
@@ -1,3 +1,8 @@
+import time
 import webbrowser
 
-webbrowser.open("https://www.youtube.com")
+for i in range(0, 3) :      
+    #time.sleep() uses seconds. 7200 seconds = 2 hours
+    time.sleep(3)
+    webbrowser.open("https://www.youtube.com")
+
