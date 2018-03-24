04c58f07f030111aa18edbe65bf2354cfeaa52b2
willimsbw <willimsbw@gmail.com>
2018-03-23 20:04:55 -0400

    Updated the url that gets opened

--- take-a-break.py
+++ take-a-break.py
@@ -10,6 +10,6 @@ print("Started timer at " + time.ctime())
 while breakCount < totalBreaks:      
     #time.sleep() uses seconds. 7200 seconds = 2 hours
     time.sleep(timeToWait)
-    webbrowser.open("https://www.youtube.com")
+    webbrowser.open("https://www.youtube.com/watch?v=ZYC1lrSdSk8")
     break_count += 1
 
