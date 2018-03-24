2765cd001b5a9aba0b5d99d83ad0cd139ada3de9
willimsbw <willimsbw@gmail.com>
2018-03-23 21:14:03 -0400

    added first step - getting list of files in prank-photos

---
+++ prank/rename-files.py
@@ -0,0 +1,9 @@
+import os
+
+def renameFiles():
+    #get files names from folde
+    fileList = os.listdir(r"c:\version-control\Learning-Python\prank\prank-photos")
+    print(fileList)
+    #for each file
+
+renameFiles()
