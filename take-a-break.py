6ff94106d61da7a0905820b2b0d7378170225216
willimsbw <willimsbw@gmail.com>
2018-03-23 20:26:50 -0400

    moved around and edited some comments

--- take-a-break.py
+++ take-a-break.py
@@ -1,14 +1,14 @@
 import time
 import webbrowser
 
+#time.sleep() uses seconds. 7200 seconds = 2 hours
 timeToWait = 3
 breakCount = 0
 totalBreaks = 3
 
-#time.ctim() gives a timestamp for current time
+#time.ctime() gives a timestamp for current time
 print("Started timer at " + time.ctime())
 while breakCount < totalBreaks:      
-    #time.sleep() uses seconds. 7200 seconds = 2 hours
     time.sleep(timeToWait)
     webbrowser.open("https://www.youtube.com/watch?v=ZYC1lrSdSk8")
     break_count += 1
