745bdf9dd14aa6bee9d1ca565f2b78ae49a5cd34
willimsbw <willimsbw@gmail.com>
2018-03-29 21:56:42 -0400

    Changed my-first-script.py to say Bryan instead of udacity student

--- my-first-script.py
+++ my-first-script.py
@@ -2,6 +2,6 @@ def welcome_message(name):
 #Prints out a personalised welcome message
     return "Welcome to this Python script, " + name + "!"
 
-#Call the welcome message function with the input "Udacity Student" 
+#Call the welcome message function with the input "Udacity Student"
 #and print the output
-print(welcome_message("Udacity Student"))
+print(welcome_message("Bryan"))
