2ebfe1b24882728cb3a31232963b0934a4d56642
willimsbw <willimsbw@gmail.com>
2018-04-01 15:42:53 -0400

    renamed variable that shared function name

--- most-prolific.py
+++ most-prolific.py
@@ -5,13 +5,13 @@ def most_prolific(albums):
     that occur the same number of times, it returns a list of years.
     years: dictionary. Holds the years albums were published and the number of
     albums published in that year
-    most_prolific_years: list. Holds the years that tied for most_prolific
-    most_prolific: integer. Holds the highest number of albums produced in a
+    most_prolific_years: list. Holds the years that tied for most albums
+    most_albums: integer. Holds the highest number of albums produced in a
     single year.
     """
     years = {} #dictionary that will hold our years and their number of occurences
-    most_prolific_years = [] #list to hold years that are most_prolific
-    most_prolific = 0 #value to hold the most albums produced in any one year
+    most_prolific_years = [] #list to hold years that had the most albums
+    most_albums = 0 #value to hold the most albums produced in any one year
 
     #for each element in our submitted dictionary, add its year to years with a
     #count of one. If the year is already in years, add 1 to the count instead
@@ -21,15 +21,15 @@ def most_prolific(albums):
         else:
             years[album] = 1
 
-    #iterate through years to find highest count and set most_prolific to that
+    #iterate through years to find highest count and set most_albums to that
     #count
     for year in years:
-        if years[year] > most_prolific:
-            most_prolific = years[year]
+        if years[year] > most_albums:
+            most_albums = years[year]
 
-    #add all years that qualify as most_prolific to most_prolific_years
+    #add all years that qualify as most_albums to most_prolific_years
     for year in years:
-        if years[year] == most_prolific:
+        if years[year] == most_albums:
             most_prolific_years.append(year)
 
     if len(most_prolific_years) == 1:
