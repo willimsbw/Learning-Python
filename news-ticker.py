dde13dc4c92573e2547fcd85e1227b3ef1263875
willimsbw <willimsbw@gmail.com>
2018-03-31 22:37:56 -0400

    Commented out my old, less-elegent solution and changed to suggested

--- news-ticker.py
+++ news-ticker.py
@@ -13,20 +13,28 @@ def create_news_ticker(headlines):
     characters long, containing as many headlines as possible with a space
     between each one.
     """
-    ticker = ""
-    for headline in headlines:
-        if len(ticker) + len(headline)+1 > 140:
+    #ticker = ""
+    #for headline in headlines:
+        #if len(ticker) + len(headline)+1 > 140:
             #if headline and a space would put ticker over the line, truncate
             #the headline, add it, and break
-            letters_to_keep = 140 - len(ticker)
-            truncated_headline = headline[:letters_to_keep]
-            ticker += truncated_headline
-            print("final ticker is: " + ticker)
+            #letters_to_keep = 140 - len(ticker)
+            #truncated_headline = headline[:letters_to_keep]
+            #ticker += truncated_headline
+            #print("final ticker is: " + ticker)
+            #break
+        #else:
+            #ticker += headline + " "
+            #print("ticker is now: " + ticker)
+
+    #return ticker
+    ticker = ""
+    for headline in headlines:
+        ticker += headline + " "
+        if len(ticker) >= 140:
+            ticker = ticker[:140]
             break
-        else:
-            ticker += headline + " "
-            print("ticker is now: " + ticker)
 
     return ticker
 
-create_news_ticker(headlines)
+print(create_news_ticker(headlines))
