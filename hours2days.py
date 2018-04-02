6613d1533d98be4ab57ac959f5fdec2f460ff742
willimsbw <willimsbw@gmail.com>
2018-04-01 20:36:23 -0400

    Created function that takes a number of hours and outputs the number of days and remaining hours they translate to as a tuple.

---
+++ hours2days.py
@@ -0,0 +1,14 @@
+def hours2days(hours):
+    """
+    Function that takes input hours and returns how many days that is,
+    and how many leftover hours. E.g., 26 hours = 1 day and 2 hours.
+    """
+
+    days = int(hours/24)
+    hours_remaining = hours%24
+
+    return (days, hours_remaining)
+
+print(hours2days(24)) # 24 hours is one day and zero hours
+print(hours2days(25)) # 25 hours is one day and one hour
+print(hours2days(10000)) # 10000 hours is 416 days and 16 hours
