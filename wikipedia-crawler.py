e9f85cf8b793c87412ba43923685733eaf413ced
Bryan Williams <Bryan.williams@achiever.com>
2018-04-03 16:06:39 -0400

    finished first fully-complete draft

--- wikipedia-crawler.py
+++ wikipedia-crawler.py
@@ -45,33 +45,37 @@ def continue_crawl(search_history, target_url, max_searches=25):
     Returns False if target_url is in search_history, search_history contains
     more than 25 values, or target_url doesn"t contain an anchor element
     search_history: list. Pages already visited
-    target_url: string. A url
+    target_url: string. The url we want to end up on
     max_searches: int. The maximum number of times you want a loop to be able to run. Defaults to 25
     """
 
-    html = requests.get(target_url).text #html from target_url as string
-    soup = BeautifulSoup(html, "html.parser") #soup object version of html for easy parsing
-
-    #if more than 25 pages checked
+    #if more than 25 pages or user-specified number checked
     if len(search_history) > max_searches:
         print("We've looked too many times!")
         return False
     #if we"ve seen this url before
-    elif target_url in search_history:
+    elif search_history[-1] in search_history[:-1]:
         print("We've already been to this page!")
         return False
     #if we hit the Philosophy page
-    elif search_history[-1] == "https://en.wikipedia.org/wiki/Philosophy":
+    elif search_history[-1] == target_url:
         print("We've hit the Philosophy page!")
         return False
     else:
         return True
 
+start_url = "https://en.wikipedia.org/wiki/Special:Random"
+target_url = "https://en.wikipedia.org/wiki/Philosophy"
+
+article_chain = [start_url]
+
 while continue_crawl(article_chain, target_url):
-    # download html of last article in article_chain
-    # find the first link in that html
+    print(article_chain[-1])
     first_link = find_first_link(article_chain[-1])
-    # add the first link to article chain
+    if not first_link:
+        print("We've hit an article with no links!")
+        break
+
     article_chain.append(first_link)
-    # delay for about two seconds
-    time.sleep(2)
+
+    time.sleep(2) #wait two second to run again to avoid DDosing wikipedia
