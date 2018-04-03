075e2c96a505f47ec94ade2cdb549a1ca63602a4
Bryan Williams <Bryan.williams@achiever.com>
2018-04-03 16:53:32 -0400

    Finished program and abstracted entire thing to a function

--- wikipedia-crawler.py
+++ wikipedia-crawler.py
@@ -3,105 +3,90 @@ from bs4 import BeautifulSoup
 import time
 import urllib
 
-def find_first_link(url):
+def crawl_wikipedia(start_url, target_url, max_pages):
     """
-    Returns the first anchor element for a given url
-    """
-    html = requests.get(url).text #html from target_url as string
-    soup = BeautifulSoup(html, "html.parser") #soup object version of html for easy parsing
+    Clicks the first link on start_url, and the first link on the next page, etc
+    until it either reaches target_url, gets stuck in a loop, or checks the predefined
+    maximum number of pages.
 
-    #if no link found, returns None. otherwise, will store the first link in the article
-    article_link = None
-
-    #variable that contains the article's body
-    content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
-
-    #for each of the direct-child <p> elements in content_div
-    for element in content_div.find_all("p", recursive=False):
-        #for each of the direct-child <a> elements in a given <p> element in content_div
-        for link in element.find_all("a", recursive=False):
-            a_href = link.get("href") #the url of that <a> element
-            if a_href.startswith("/wiki"):
-                if a_href.startswith("/wiki/Help"):
-                    #we don't want to link to help articles
-                    continue
-                else:
-                    #make first_link the current element"s first <a>"s href and
-                    #stop looking
-                    article_link = a_href
-                    break
-
-        #don't go to the next element, now that we have a first_link or have looked
-        #at every link option
-        break
-
-    #convert relative link from wikipedia to a full URL
-    first_link = urllib.parse.urljoin('https://en.wikipedia.org/', article_link)
-
-    return first_link
-
-def continue_crawl(search_history, target_url, max_searches=25):
-    """
-    Returns False if target_url is in search_history, search_history contains
-    more than 25 values, or target_url doesn"t contain an anchor element
-    search_history: list. Pages already visited
-    target_url: string. The url we want to end up on
-    max_searches: int. The maximum number of times you want a loop to be able to run. Defaults to 25
+    start_url: string. The first wikipedia article you want the crawler to look at.
+    target_url: string. The wikipedia article you want the crawler to try to reach.
+    max_pages: int. The maximum number of pages you want the crawler to look at
     """
 
-    #if more than 25 pages or user-specified number checked
-    if len(search_history) > max_searches:
-        print("We've looked too many times!")
-        return False
-    #if we"ve seen this url before
-    elif search_history[-1] in search_history[:-1]:
-        print("We've already been to this page!")
-        return False
-    #if we hit the Philosophy page
-    elif search_history[-1] == target_url:
-        print("We've hit the Philosophy page!")
-        return False
-    else:
-        return True
+    def find_first_link(url):
+        """
+        Returns the first anchor element for a given url
+        """
+        html = requests.get(url).text #html from target_url as string
+        soup = BeautifulSoup(html, "html.parser") #soup object version of html for easy parsing
 
-start_url = "https://en.wikipedia.org/wiki/Special:Random"
-target_url = "https://en.wikipedia.org/wiki/Philosophy"
+        #if no link found, returns None. otherwise, will store the first link in the article
+        article_link = None
 
-article_chain = [start_url]
+        #variable that contains the article's body
+        content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
 
-while continue_crawl(article_chain, target_url):
-    print(article_chain[-1])
-    first_link = find_first_link(article_chain[-1])
-    if not first_link:
-        print("We've hit an article with no links!")
-        break
+        #for each of the direct-child <p> elements in content_div
+        for element in content_div.find_all("p", recursive=False):
+            #for each of the direct-child <a> elements in a given <p> element in content_div
+            for link in element.find_all("a", recursive=False):
+                a_href = link.get("href") #the url of that <a> element
+                if a_href.startswith("/wiki"):
+                    if a_href.startswith("/wiki/Help"):
+                        #we don't want to link to help articles
+                        continue
+                    else:
+                        #make first_link the current element"s first <a>"s href and
+                        #stop looking
+                        article_link = a_href
+                        break
+            #don't go to the next element, if we've found an article_link
+            if article_link:
+                break
 
-    article_chain.append(first_link)
+        if not article_link:
+            return None
+        else:
+            #convert relative link from wikipedia to a full URL
+            first_link = urllib.parse.urljoin('https://en.wikipedia.org/', article_link)
+            return first_link
 
-    time.sleep(2) #wait two second to run again to avoid DDosing wikipedia
+    def continue_crawl(search_history, target_url, max_searches=25):
+        """
+        Returns False if target_url is in search_history, search_history contains
+        more than 25 values, or target_url doesn"t contain an anchor element
+        search_history: list. Pages already visited
+        target_url: string. The url we want to end up on
+        max_searches: int. The maximum number of times you want a loop to be able to run. Defaults to 25
+        """
 
-article_chain = [start_url]
+        #if more than 25 pages or user-specified number checked
+        if len(search_history) > max_searches:
+            print("We've looked too many times!")
+            return False
+        #if we"ve seen this url before
+        elif search_history[-1] in search_history[:-1]:
+            print("We've already been to this page!")
+            return False
+        #if we hit the Philosophy page
+        elif search_history[-1] == target_url:
+            print("We've hit the desired page: " + target_url)
+            return False
+        else:
+            return True
 
-while continue_crawl(article_chain, target_url, 50):
-    print(article_chain[-1])
-    first_link = find_first_link(article_chain[-1])
-    if not first_link:
-        print("We've hit an article with no links!")
-        break
+    article_chain = [start_url]
 
-    article_chain.append(first_link)
+    while continue_crawl(article_chain, target_url, max_pages):
+        print(article_chain[-1])
+        first_link = find_first_link(article_chain[-1])
+        if not first_link:
+            print("We've hit an article with no links!")
+            break
 
-    time.sleep(2) #wait two second to run again to avoid DDosing wikipedia
+        article_chain.append(first_link)
 
-article_chain = [start_url]
+        time.sleep(2) #wait two second to run again to avoid DDosing wikipedia
 
-while continue_crawl(article_chain, target_url, 10):
-    print(article_chain[-1])
-    first_link = find_first_link(article_chain[-1])
-    if not first_link:
-        print("We've hit an article with no links!")
-        break
-
-    article_chain.append(first_link)
-
-    time.sleep(2) #wait two second to run again to avoid DDosing wikipedia
+crawl_wikipedia("https://en.wikipedia.org/wiki/Special:Random", "https://en.wikipedia.org/wiki/Knowledge", 15)
