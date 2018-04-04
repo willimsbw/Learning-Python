bec6d310a6da45e4b5a01c56fa0060babb54cc12
Bryan Williams <Bryan.williams@achiever.com>
2018-04-04 11:27:23 -0400

    Created outline for draw-a-square.py

---
+++ draw-a-square.py
@@ -0,0 +1,12 @@
+import tutrle
+
+def drawSquare():
+    """
+    Draws a single square
+    """
+    #move forward, then turn right. Repeat 4 times
+    for i in range(4):
+        #move forward
+        #turn right
+
+drawSquare()
