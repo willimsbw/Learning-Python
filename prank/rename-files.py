10d0607b316d3ee63cb0002f3d319d759a84223a
Bryan Williams <Bryan.williams@achiever.com>
2018-04-04 10:34:24 -0400

    Added for loop to rename files

--- prank/rename-files.py
+++ prank/rename-files.py
@@ -1,9 +1,19 @@
 import os
+import string
 
 def renameFiles():
-    #get files names from folde
-    fileList = os.listdir(r"c:\version-control\Learning-Python\prank\prank-photos")
+    #get files names from folder
+    fileList = os.listdir(r"c:/version-control/Learning-Python/prank/prank-photos")
     print(fileList)
     #for each file
+    for fileName in fileList:
+        #will delete  any numerical characters when used in str.translate()
+        transTable = str.maketrans("1234567890", "          ", "1234567890")
+        #new name is the file name with any numerical characters deleted
+        newName = fileName.translate(transTable)
+        os.rename(fileName, newName)
+
+    print("renamed:")
+    print(fileList)
 
 renameFiles()
