eb532977e5c347b0e7acffa602dde6a9844ddc54
Bryan Williams <Bryan.williams@achiever.com>
2018-04-04 17:12:08 -0400

    Customized the look of my drawing and my turtle

--- draw-a-square.py
+++ draw-a-square.py
@@ -6,16 +6,31 @@ def drawSquare():
     """
     #create a turtle named brad
     brad = turtle.Turtle()
-    #create a window for brad to draw on
+    #open a window for brad to draw on
     window = turtle.Screen()
     #color window background red
     window.bgcolor("red")
+    #color the turtle white
+    brad.color("white")
+    #color the lines white
+    brad.pencolor("white")
+    #color the filling of the square white
+    brad.fillcolor("white")
+    #Make brad a circle
+    brad.shape("circle")
+
+    #tell it to fill the square that's created with our pre-determined color
+    brad.begin_fill()
+
     #move forward, then turn right. Repeat 4 times
     for i in range(4):
-        #move forward 100 steps
+        #move forward 100 pixels
         brad.forward(100)
         #turn right 90 degrees
         brad.right(90)
+
+    #Tells it that the shape is made so apply the filling
+    brad.end_fill()
 
     #tell our window to close when clicked on
     window.exitonclick()
