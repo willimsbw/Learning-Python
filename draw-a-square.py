f7590cfaf35dc36a7471170f952040dfb575a0d8
Bryan Williams <Bryan.williams@achiever.com>
2018-04-04 11:52:32 -0400

    Opens window and turtle draws square.

--- draw-a-square.py
+++ draw-a-square.py
@@ -1,12 +1,23 @@
-import tutrle
+import turtle
 
 def drawSquare():
     """
     Draws a single square
     """
+    #create a turtle named brad
+    brad = turtle.Turtle()
+    #create a window for brad to draw on
+    window = turtle.Screen()
+    #color window background red
+    window.bgcolor("red")
     #move forward, then turn right. Repeat 4 times
     for i in range(4):
-        #move forward
-        #turn right
+        #move forward 100 steps
+        brad.forward(100)
+        #turn right 90 degrees
+        brad.right(90)
+
+    #tell our window to close when clicked on
+    window.exitonclick()
 
 drawSquare()
