78b6fba5c072785cf18b6e18a0ce5223e1c2f8b6
Bryan Williams <Bryan.williams@achiever.com>
2018-04-05 12:42:24 -0400

    Abstraced test cases to a function

--- turtle-mini-project.py
+++ turtle-mini-project.py
@@ -53,25 +53,26 @@ def drawKochWhole(sideLength, n, someTurtle, numSides, reverse=False):
 
     someTurtle.end_fill()
 
-brad = turtle.Turtle()
-window = turtle.Screen()
-#increase drawing surface size and make brad faster
-window.screensize(5000, 5000)
-brad.speed("fastest")
-#brad only shows every 100th update as he draws - speeds up even more
-window.tracer(100)
+def drawSomeKochs():
+    brad = turtle.Turtle()
+    window = turtle.Screen()
+    #increase drawing surface size and make brad faster
+    window.screensize(5000, 5000)
+    brad.speed("fastest")
+    #brad only shows every 100th update as he draws - speeds up even more
+    window.tracer(100)
 
-drawKochSide(200, 3, brad)
-window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
-time.sleep(5)
-window.resetscreen()
-drawKochWhole(300, 2, brad, 15)
-window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
-time.sleep(5)
-window.resetscreen()
-drawKochWhole(1000, 5, brad, 10)
-window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
+    drawKochSide(200, 3, brad)
+    window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
+    time.sleep(5)
+    window.resetscreen()
+    drawKochWhole(300, 2, brad, 15)
+    window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
+    time.sleep(5)
+    window.resetscreen()
+    drawKochWhole(1000, 5, brad, 10)
+    window.update() #updates any of the drawing our tracer missed (the last <100 lines drawn)
 
+    window.exitonclick()
 
-
-window.exitonclick()
+drawSomeKochs()
