4fc5025a1350e9fd6c090b658056607a76010007
Bryan Williams <Bryan.williams@achiever.com>
2018-04-06 14:57:51 -0400

    made a function to print all attributes of a movie object for test cases

--- heavily-guided-exercises/movie-website/movie-website.py
+++ heavily-guided-exercises/movie-website/movie-website.py
@@ -1,21 +1,21 @@
 import movies
 
+def print_all_movie_attributes(object):
+    print("title: " + object.title +
+    "\nplot summary: " + object.plot_summary +
+    "\ntrailer url: " + object.trailer_url +
+    "\nposwer image url: " + object.poster_image_url)
+
+
 toy_story = movies.Movie("Toy Story",
                         "toys are alive!",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc",
                         "https://vignette.wikia.nocookie.net/pixar/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20160329080002")
-
-print(toy_story.title)
-print(toy_story.plot_summary)
-print(toy_story.trailer_url)
-print(toy_story.poster_image_url)
 
 avatar = movies.Movie("Avatar",
                     "blue alien native americans get dances with wolves'd by white people",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY",
                     "https://www.movieposter.com/posters/archive/main/98/MPW-49246")
 
-print(avatar.title)
-print(avatar.plot_summary)
-print(avatar.trailer_url)
-print(avatar.poster_image_url)
+print_all_movie_attributes(toy_story)
+print_all_movie_attributes(avatar)
