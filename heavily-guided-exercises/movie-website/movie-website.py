6250d9a099a1747fdc94cd7b232102c508d401b8
Bryan Williams <Bryan.williams@achiever.com>
2018-04-06 14:52:17 -0400

    reordered Movie() inputs for readability and fixed objects in movies.py to match

--- heavily-guided-exercises/movie-website/movie-website.py
+++ heavily-guided-exercises/movie-website/movie-website.py
@@ -1,15 +1,21 @@
 import movies
 
-toy_story = movies.Movie("toys are alive!", "https://www.youtube.com/watch?v=KYz2wyBy3kc", "https://vignette.wikia.nocookie.net/pixar/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20160329080002", "Toy Story")
+toy_story = movies.Movie("Toy Story",
+                        "toys are alive!",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc",
+                        "https://vignette.wikia.nocookie.net/pixar/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20160329080002")
 
+print(toy_story.title)
 print(toy_story.plot_summary)
 print(toy_story.trailer_url)
 print(toy_story.poster_image_url)
-print(toy_story.title)
 
-avatar = movies.Movie("blue alien native americans get dances with wolves'd by white people", "https://www.youtube.com/watch?v=5PSNL1qE6VY", "https://www.movieposter.com/posters/archive/main/98/MPW-49246", "Avatar")
+avatar = movies.Movie("Avatar",
+                    "blue alien native americans get dances with wolves'd by white people",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+                    "https://www.movieposter.com/posters/archive/main/98/MPW-49246")
 
+print(avatar.title)
 print(avatar.plot_summary)
 print(avatar.trailer_url)
 print(avatar.poster_image_url)
-print(avatar.title)
--- heavily-guided-exercises/movie-website/movies.py
+++ heavily-guided-exercises/movie-website/movies.py
@@ -1,6 +1,6 @@
 class Movie():
 
-    def __init__(self, plot_summary, movie_trailer, poster_image, movie_title):
+    def __init__(self, movie_title, plot_summary, movie_trailer, poster_image):
       self.title = movie_title
       self.trailer_url = movie_trailer
       self.poster_image_url = poster_image
