090265b3838a172ed917a274932b5d4b4fcabe35
willimsbw <willimsbw@gmail.com>
2018-04-05 21:15:43 -0400

    Added test cases and regex for finding curse words

--- mini-projects/profanity-checker/check-profanity.py
+++ mini-projects/profanity-checker/check-profanity.py
@@ -1,6 +1,14 @@
+import re
+
 def readText(file):
     quotes = open(file)
     contents = quotes.read()
     print(contents)
+    regex = re.compile(r"fuck|shit|ass|dick|piss|cunt|damn|bastard|bitch|hell", re.IGNORECASE)
+    list = regex.findall(contents)
+    print(list)
+
+
+
 
 readText("C:/version-control/learning-python/mini-projects/profanity-checker/movie_quotes.txt")
