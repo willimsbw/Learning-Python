95d3cb8058bd002bf79a713021094f3abc3df0bd
Bryan Williams <Bryan.williams@achiever.com>
2018-04-06 16:27:51 -0400

    Added class variable valid_ratings

--- heavily-guided-exercises/movie-website/movies.py
+++ heavily-guided-exercises/movie-website/movies.py
@@ -2,6 +2,8 @@ import webbrowser
 
 class Movie():
 
+    valid_ratings = ["G", "PG", "PG-13", "R"]
+
     def __init__(self, movie_title, plot_summary, movie_trailer, poster_image):
       self.title = movie_title
       self.trailer_youtube_url = movie_trailer
