d0d67995dc45cdc8a5fbefc0056a7ec8a21e5765
willimsbw <willimsbw@gmail.com>
2018-04-05 21:19:41 -0400

    Moved profanity checker into its own function

--- mini-projects/profanity-checker/check-profanity.py
+++ mini-projects/profanity-checker/check-profanity.py
@@ -4,11 +4,10 @@ def readText(file):
     quotes = open(file)
     contents = quotes.read()
     print(contents)
+
+def checkProfanity(string):
     regex = re.compile(r"fuck|shit|ass|dick|piss|cunt|damn|bastard|bitch|hell", re.IGNORECASE)
-    list = regex.findall(contents)
+    list = regex.findall(string)
     print(list)
-
-
-
 
 readText("C:/version-control/learning-python/mini-projects/profanity-checker/movie_quotes.txt")
