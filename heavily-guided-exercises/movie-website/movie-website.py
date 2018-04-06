5ee3dc4879c727f3cfa4798efb98efd67a356f5c
Bryan Williams <Bryan.williams@achiever.com>
2018-04-06 14:45:08 -0400

    built out test case fully and changed a variable name in class

--- heavily-guided-exercises/movie-website/movie-website.py
+++ heavily-guided-exercises/movie-website/movie-website.py
@@ -2,4 +2,7 @@ import movies
 
 toy_story = movies.Movie("toys are alive!", "https://www.youtube.com/watch?v=KYz2wyBy3kc", "https://vignette.wikia.nocookie.net/pixar/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20160329080002", "Toy Story")
 
-print(toy_story)
+print(toy_story.plot_summary)
+print(toy_story.trailer_url)
+print(toy_story.poster_image_url)
+print(toy_story.title)
--- heavily-guided-exercises/movie-website/movies.py
+++ heavily-guided-exercises/movie-website/movies.py
@@ -2,6 +2,6 @@ class Movie():
 
     def __init__(self, plot_summary, movie_trailer, poster_image, movie_title):
       self.title = movie_title
-      self.trailer = movie_trailer
+      self.trailer_url = movie_trailer
       self.poster_image_url = poster_image
       self.plot_summary = plot_summary
