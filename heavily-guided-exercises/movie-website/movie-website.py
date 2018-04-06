eec0e1cf364e26e4e2ec8abfd7db370cb63f990f
Bryan Williams <Bryan.williams@achiever.com>
2018-04-06 14:42:44 -0400

    Created basic class definition and constructor for Movie() and a test case.

---
+++ heavily-guided-exercises/movie-website/movie-website.py
@@ -0,0 +1,5 @@
+import movies
+
+toy_story = movies.Movie("toys are alive!", "https://www.youtube.com/watch?v=KYz2wyBy3kc", "https://vignette.wikia.nocookie.net/pixar/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20160329080002", "Toy Story")
+
+print(toy_story)
---
+++ heavily-guided-exercises/movie-website/movies.py
@@ -0,0 +1,7 @@
+class Movie():
+
+    def __init__(self, plot_summary, movie_trailer, poster_image, movie_title):
+      self.title = movie_title
+      self.trailer = movie_trailer
+      self.poster_image_url = poster_image
+      self.plot_summary = plot_summary
